0ad6556c8d15d4f7a2c66e8a0fe42ef5151acb75
Tim Backus <tbackus127@gmail.com>
2017-03-10 20:55:25 -0500

    Script numbering now works.
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

--- src/com/rath/jvn/editor/scene/SceneScriptHeaderPanel.java
+++ src/com/rath/jvn/editor/scene/SceneScriptHeaderPanel.java
@@ -11,6 +11,12 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 
+/**
+ * This class acts as a wrapper for the script.
+ * 
+ * @author Tim Backus tbackus127@gmail.com
+ *
+ */
 public class SceneScriptHeaderPanel extends JPanel {
 
   /** Serial Version UID. */
@@ -19,10 +25,22 @@ public class SceneScriptHeaderPanel extends JPanel {
   /** Default message for a new quote in the script. */
   private static final String DEFAULT_MESSAGE = "---";
 
-  public SceneScriptHeaderPanel(final JList<String> list, final DefaultListModel<String> listMod) {
+  /** Handle to the list model. */
+  private final DefaultListModel<String> listMod;
+
+  /**
+   * Default constructor.
+   * 
+   * @param list the script as a JList of Strings.
+   * @param listMod the list model.
+   */
+  public SceneScriptHeaderPanel(final JList<String> list, final DefaultListModel<String> lm) {
     super();
+
+    this.listMod = lm;
     setLayout(new FlowLayout(FlowLayout.LEFT));
 
+    // Label
     final JLabel scriptLabel = new JLabel("  Script");
     add(scriptLabel);
 
@@ -44,6 +62,7 @@ public class SceneScriptHeaderPanel extends JPanel {
           listMod.add(index, DEFAULT_MESSAGE);
 
         }
+        renumberList();
       }
 
     });
@@ -57,14 +76,43 @@ public class SceneScriptHeaderPanel extends JPanel {
       public void actionPerformed(ActionEvent evt) {
 
         final int index = list.getSelectedIndex();
-        
-        if(index >= 0) {
+
+        if (index >= 0) {
           listMod.remove(index);
+          renumberList();
         }
       }
 
     });
 
     add(rmButton);
+  }
+
+  /**
+   * Renumbers the script.
+   */
+  private void renumberList() {
+    for (int i = 0; i < this.listMod.size(); i++) {
+
+      final String originalStr = listMod.getElementAt(i);
+      
+      // If the quote is already numbered
+      if(originalStr.matches("\\d+:\\s+.*")) {
+        
+        // Numbered wrong
+        if(!originalStr.startsWith("" + i + ": ")) {
+          final int colIndex = originalStr.indexOf(": ");
+          final String newStr = ("" + i + ": ") + originalStr.substring(colIndex + 2);
+          listMod.remove(i);
+          listMod.add(i, newStr);
+          continue;
+        }
+        continue;
+      }
+      
+      // Not numbered
+      listMod.remove(i);
+      listMod.add(i, ("" + i + ": ") + originalStr);
+    }
   }
 }
